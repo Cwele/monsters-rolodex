ef960a762358bb812a83368317639658d913703f
Delani <delanicwele68@gmail.com>
2020-05-04 12:42:11 +0200

    Add files via upload
    Adding files

---
+++ build/precache-manifest.b4dc81fe809e4b0fa3c49e52059f4ef7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d911312920b95e7194412a688337dd5f",
+    "url": "/https//Cwele.github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cb380c3938ed8af840c3",
+    "url": "/https//Cwele.github.io/monsters-rolodex/static/css/main.a76dc2c6.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/https//Cwele.github.io/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/https//Cwele.github.io/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb380c3938ed8af840c3",
+    "url": "/https//Cwele.github.io/monsters-rolodex/static/js/main.a15f7d62.chunk.js"
+  },
+  {
+    "revision": "ba9a2eb7bc1a7aeeb510",
+    "url": "/https//Cwele.github.io/monsters-rolodex/static/js/runtime-main.ca088673.js"
+  }
+]);
